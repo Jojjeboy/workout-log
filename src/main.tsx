03e570da9574f3e3839d25269348ee02e52f1f90
Jojjeboy <karnstamjohannes@gmail.com>
2025-12-02 22:53:47 +0100

    feat: setup CI/CD 3

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { MantineProvider } from '@mantine/core';
-import { Notifications } from '@mantine/notifications';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import { theme } from './theme';
-import '@mantine/core/styles.css';
-import '@mantine/dates/styles.css';
-import './i18n';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { MantineProvider } from "@mantine/core";
+import { Notifications } from "@mantine/notifications";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// Ändring 1: Byt BrowserRouter till HashRouter
+import { HashRouter } from "react-router-dom";
+import App from "./App";
+import { theme } from "./theme";
+import "@mantine/core/styles.css";
+import "@mantine/dates/styles.css";
+import "./i18n";
+import "./index.css";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -20,14 +21,15 @@ const queryClient = new QueryClient({
   },
 });
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <MantineProvider theme={theme} forceColorScheme="light">
-        <BrowserRouter>
+        {/* Ändring 2: Byt <BrowserRouter> till <HashRouter> */}
+        <HashRouter>
           <App />
           <Notifications />
-        </BrowserRouter>
+        </HashRouter>
       </MantineProvider>
     </QueryClientProvider>
   </React.StrictMode>
